6fc6e60679a6a183170ddd53302e3920e9066d45
Tiel Van Hecke <tiel.vhecke@hotmail.com>
2020-08-11 17:15:10 +0200

    Implement EventsViewModel

--- app/src/main/java/com/example/android/eventhub/ui/events/EventsViewModel.kt
+++ app/src/main/java/com/example/android/eventhub/ui/events/EventsViewModel.kt
@@ -1,8 +1,45 @@
 package com.example.android.eventhub.ui.events
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.eventhub.getDatabase
+import com.example.android.eventhub.repository.EventRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 class EventsViewModel(application: Application): ViewModel() {
+    private val eventRepository = EventRepository(getDatabase(application))
 
+    val events = eventRepository.events
+
+    private val viewModelJob = SupervisorJob()
+
+    private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    private val _eventNetworkError = MutableLiveData<Boolean>()
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+    init {
+        refreshDataFromNetwork()
+    }
+
+    private fun refreshDataFromNetwork() = viewModelScope.launch {
+        try {
+            eventRepository.refreshEvents()
+            _eventNetworkError.value = false
+        } catch (networkError: IOException) {
+            _eventNetworkError.value = true
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 }
