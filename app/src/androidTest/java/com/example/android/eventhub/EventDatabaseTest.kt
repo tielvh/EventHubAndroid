6adccbb74106b0d4970ecaf25b5339192fb2f53b
Tiel Van Hecke <tiel.vhecke@hotmail.com>
2020-08-11 15:58:16 +0200

    Add basic database test

---
+++ app/src/androidTest/java/com/example/android/eventhub/EventDatabaseTest.kt
@@ -0,0 +1,46 @@
+package com.example.android.eventhub
+
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.android.eventhub.database.EventDatabase
+import com.example.android.eventhub.database.daos.EventDao
+import com.example.android.eventhub.domain.Event
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+import java.lang.Exception
+import java.time.LocalDateTime
+
+@RunWith(AndroidJUnit4::class)
+class EventDatabaseTest {
+    private lateinit var eventDao: EventDao
+    private lateinit var db: EventDatabase
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        db = Room.inMemoryDatabaseBuilder(context, EventDatabase::class.java)
+            .allowMainThreadQueries().build()
+        eventDao = db.eventDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndGetEvent() {
+        val event =
+            Event(1, "Test", LocalDateTime.of(2020, 8, 11, 12, 0, 0), "here", "test", "test")
+        eventDao.insert(listOf(event))
+        val thisEvent = eventDao.getOne(1)
+        assertEquals(event.name, thisEvent?.name)
+    }
+}
