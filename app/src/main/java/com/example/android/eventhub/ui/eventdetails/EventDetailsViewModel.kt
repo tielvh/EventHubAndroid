b719774c278d9ad3b43922c43a303aed726741f6
Tiel Van Hecke <tiel.vhecke@hotmail.com>
2020-08-12 15:34:50 +0200

    Add comments to EventDetailsViewModel

--- app/src/main/java/com/example/android/eventhub/ui/eventdetails/EventDetailsViewModel.kt
+++ app/src/main/java/com/example/android/eventhub/ui/eventdetails/EventDetailsViewModel.kt
@@ -5,10 +5,24 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.example.android.eventhub.domain.Comment
 import com.example.android.eventhub.domain.Event
+import com.example.android.eventhub.getDatabase
+import com.example.android.eventhub.repository.CommentRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import java.io.IOException
 import java.time.format.DateTimeFormatter
 
 class EventDetailsViewModel(application: Application, e: Event) : ViewModel() {
+    private val commentRepository = CommentRepository(getDatabase(application))
+
+    private val viewModelJob = SupervisorJob()
+
+    private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
     private val _event = MutableLiveData<Event>()
     val event: LiveData<Event>
         get() = _event
@@ -21,7 +35,30 @@ class EventDetailsViewModel(application: Application, e: Event) : ViewModel() {
         event.dateTime.format(DateTimeFormatter.ofPattern("HH:mm"))
     }
 
+    private val _comments = MutableLiveData<List<Comment>>()
+    val comments: LiveData<List<Comment>>
+        get() = _comments
+
     init {
         _event.value = e
+    }
+
+    private fun refreshCommentsFromNetwork() = viewModelScope.launch {
+        event.value?.let {
+            try {
+                commentRepository.refreshComments(it.id)
+            } catch (networkError: IOException) {
+                // Do not display network error message to avoid cluttering the UI
+            }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    fun onRefreshComments() {
+        refreshCommentsFromNetwork()
     }
 }
