b8c1ef9679324ca030af422f7a4d88588ea9d5bf
Tiel Van Hecke <tiel.vhecke@hotmail.com>
2020-08-12 17:20:31 +0200

    Add custom options menu

--- app/src/main/java/com/example/android/eventhub/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/eventhub/ui/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.eventhub.R
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +19,9 @@ class MainActivity : AppCompatActivity() {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu_default, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, this.findNavController(R.id.navHostFragment)) || super.onOptionsItemSelected(item)
     }
 }
